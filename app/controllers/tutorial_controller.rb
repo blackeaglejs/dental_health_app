627a3a4c6617ce3a6ee9e8e89d6adc7a7450d983
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-04-04 15:41:09 -0400

    Adding all styling

--- app/controllers/tutorial_controller.rb
+++ app/controllers/tutorial_controller.rb
@@ -1,29 +1,65 @@
 class TutorialController < ApplicationController
 	def babyteeth
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_intro
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_form_1
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_form_2
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_form_3
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_form_4
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_prev_1
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_prev_2
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 	def cav_prev_3
+		@user = current_user
+		if @user
+			@profile = @user.profile
+		end
 	end
 
 end
